c34c46bf596d22b9516b9ca2563ea988ed9139ac
MH <gmhmhmhg.dev@gmail.com>
2023-07-03 21:42:40 +0900

    ---

---
+++ src/main/java/com/zb/catchtable/CatchTableApplication.java
@@ -0,0 +1,13 @@
+package com.zb.catchtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchTableApplication.class, args);
+    }
+
+}
